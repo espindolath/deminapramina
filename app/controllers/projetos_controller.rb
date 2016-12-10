e3376c614d400b0ef1ff940489772be41f1b5e6d
Mari Zancanaro <mariana.silva.zancanaro@gmail.com>
2016-12-10 15:27:44 -0200

    form com banco de dados

--- app/controllers/projetos_controller.rb
+++ app/controllers/projetos_controller.rb
@@ -2,4 +2,17 @@ class ProjetosController < ApplicationController
   def new 
       @projeto = Projeto.new
   end
+    def create
+     @projeto = Projeto.new(projeto_params)
+     @projeto.save
+    end
+    
+    private
+    
+    def projeto_params
+        params
+            .require(:projeto)
+            .permit(:nome, :descricao, :contato, :organizador, :foto)
+    
+    end
 end
